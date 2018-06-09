35a63a1efea26fbe227a1a46e2c9451b323e6173
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2018-06-09 17:51:26 +0200

    first version

---
+++ components/container.js
@@ -0,0 +1,14 @@
+/* global React */
+import { $div } from '../../common/tags';
+
+export class Container extends React.Component {
+  render() {
+    const { children, style } = this.props;
+    const classes = (this.props.className || '').split(' ');
+    classes.push('container');
+    const props = { className: classes.join(' ').trim(), style };
+    return $div(props, ...([].concat(children)));
+  }
+}
+export const $Container = (props, ...children) =>
+  React.createElement(Container, props, ...children);
---
+++ index.js
@@ -0,0 +1,24 @@
+/* global React */
+export const $h1 = (props, text) => React.createElement('h1', props, text);
+export const $h2 = (props, text) => React.createElement('h2', props, text);
+export const $h3 = (props, text) => React.createElement('h3', props, text);
+export const $h4 = (props, text) => React.createElement('h4', props, text);
+export const $h5 = (props, text) => React.createElement('h5', props, text);
+export const $div = (props, ...children) => React.createElement('div', props, ...children);
+export const $span = (props, ...children) => React.createElement('span', props, ...children);
+export const $p = (props, ...children) => React.createElement('p', props, ...children);
+export const $button = (props, ...children) => React.createElement('button', props, ...children);
+export const $img = (props, ...children) => React.createElement('img', props, ...children);
+export const $a = (props, ...children) => React.createElement('a', props, ...children);
+export const $ul = (props, ...children) => React.createElement('ul', props, ...children);
+export const $li = (props, ...children) => React.createElement('li', props, ...children);
+export const $nav = (props, ...children) => React.createElement('nav', props, ...children);
+export const $br = (props, ...children) => React.createElement('br', props, ...children);
+export const $footer = (props, ...children) => React.createElement('footer', props, ...children);
+export const $label = (props, ...children) => React.createElement('label', props, ...children);
+export const $input = (props, ...children) => React.createElement('input', props, ...children);
+export const $form = (props, ...children) => React.createElement('form', props, ...children);
+export const $section = (props, ...children) => React.createElement('section', props, ...children);
+export const $i = (props, ...children) => React.createElement('i', props, ...children);
+export const $aside = (props, ...children) => React.createElement('aside', props, ...children);
+export const $article = (props, ...children) => React.createElement('article', props, ...children);
